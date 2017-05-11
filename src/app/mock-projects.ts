13918beb36ea30424255b1be1bb3e3073609cd8a
Julio Gutierrez <juliog@knights.ucf.edu>
2017-05-11 03:20:59 -0400

    Updated mock data

--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -4,29 +4,13 @@ import { AdjacencyList } from './graph';
 export const SOLUTION_NAME: string = "MyProject";
 
 export const PROJECTS: Project[] = [
-    { id: 0, name: "solver", type: ProjectType.StaticLibrary },
-    { id: 1, name: "tool", type: ProjectType.Exectuable },
-    { id: 2, name: "client", type: ProjectType.Exectuable },
+    { id: 0, name: "mylib", type: ProjectType.StaticLibrary },
+    { id: 1, name: "app", type: ProjectType.Exectuable },
     { id: 3, name: "tests", type: ProjectType.Exectuable }
 ];
 
 export const MAKE_PROJECTS: MakeThirdPartyProject[] = [
-    {
-      id: 4,
-      name: "mongoc",
-      type: ProjectType.SharedLibrary,
-      sourceType: ThirdPartySource.File,
-      location: "https://github.com/mongodb/mongo-c-driver/releases/download/1.6.1/mongo-c-driver-1.6.1.tar.gz",
-      configureCommand: `\${SOURCE_DIR}/configure
-  --disable-automatic-init-and-cleanup
-  --enable-tests=no
-  --enable-examples=no
-  --with-libbson=bundled
-  --prefix=\${THIRD_PARTY_INSTALL_PREFIX}
-`,
-      buildCommand: "make",
-      installCommand: "make install"
-    }
+
 ];
 
 export const CMAKE_PROJECTS: CMakeThirdPartyProject[] = [
@@ -44,16 +28,12 @@ export const CMAKE_PROJECTS: CMakeThirdPartyProject[] = [
 ];
 
 export const DEPENDENCY_GRAPH = new Map<number, Array<number>>([
-// 0 = solver has no dependencies
+// 0 = mylib has no dependencies
 [0, []],
-// 1 = tool depends solver
+// 1 = app depends on mylib
 [1, [0]],
-// 2 = client depends on solver and mongoc
-[2, [0, 4]],
-// 3 = tests depend on solver and gtest
+// 3 = tests depend on mylib and gtest
 [3, [0, 5]],
-// 4 = mongoc depends on bsonc, but that comes bundled
-[4, []],
 // 5 = gtest
 [5, []]
 ]);
