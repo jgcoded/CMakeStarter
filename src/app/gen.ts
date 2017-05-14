c119ec4c993b308777f3af3d339a66441a563dec
Julio Gutierrez <juliog@knights.ucf.edu>
2017-05-14 17:55:48 -0400

    Added documentation to generated cmake code

--- src/app/gen.ts
+++ src/app/gen.ts
@@ -11,39 +11,72 @@ import {
 export function GenerateRootCMakeListsTxt(solutionName: string): string {
   let template: string =
     `
+# Sets the minimum required version of CMake needed to process this file.
 cmake_minimum_required(VERSION 2.8.11)
 
+# This command sets the name for the entire project.
 project(${solutionName})
 
+# These commands define variables that are used throughout other CMake files in this project.
 set(${solutionName}_ROOT \${CMAKE_CURRENT_LIST_DIR})
 set(${solutionName}_BIN_DIR \${${solutionName}_ROOT}/bin)
 set(${solutionName}_LIB_DIR \${${solutionName}_ROOT}/lib)
 set(${solutionName}_CMAKE_DIR \${${solutionName}_ROOT}/cmake)
 set(${solutionName}_SUBMODULES_DIR \${${solutionName}_ROOT}/submodules)
 
+# Change where CMake should output compiled libraries and executables.
+# This is based on preference, so these set() commands should be changed
+# or removed.
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY \${${solutionName}_LIB_DIR})
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY \${${solutionName}_BIN_DIR})
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY \${${solutionName}_BIN_DIR})
 
-set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} \${${solutionName}_CMAKE_DIR})
-
+# Set the C++ standard version to C++11. On older versions of CMake the
+# CMAKE_CXX_FLAGS must be changed instead.
 set(CMAKE_CXX_STANDARD 11)
 if(\${CMAKE_MAJOR_VERSION}.\${CMAKE_MINOR_VERSION} LESS 3.1)
     set(CMAKE_CXX_FLAGS "\${CMAKE_CXX_FLAGS} -std=c++11")
 endif()
 
-#Use solution folders
+# Use solution folders. In Visual Studio this will place a solution within a folder.
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+# Auto-generated files are placed in a "Generated" folder.
 set_property(GLOBAL PROPERTY AUTOGEN_TARGETS_FOLDER "Generated")
 
+# Export the commands CMake used to compile the project. This is useful for tools
+# like Vim's YouCompleteMe.
 set(CMAKE_EXPORT_COMPILE_COMMANDS "ON")
 
+# Search for CMake files in the cmake/ directory as well
+set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} \${${solutionName}_CMAKE_DIR})
+
+# This command will load and run CMake code in the 3rdParty.cmake file within
+# the cmake/ folder. CMake is able to find this file because the
+# CMAKE_MODULE_PATH variable was changed to include the cmake/ folder.
 include(3rdParty)
 
+# Specify to include the lib/ folder in the paths the linker should look for
+# libraries. This command is usually not necessary.
 link_directories(lib/)
 
+# Adds the src/ directory to the build. This command expects that the src/
+# subdirectory contains a CMakeLists.txt file. CMake processes that file
+# before continuing processing this file.
 add_subdirectory(src/)
 
+# When installing, this command will place a .cmake file within the cmake/
+# directory where the project will be installed. This .cmake file can be
+# used by an outside project to reference the targets within this project
+# as if the outside project were building this project from its own build
+# tree.
+#
+# The outside project just has to include the .cmake file using the include()
+# command, and then it can reference the targets defined in this project
+# with a concatenation of the namespace and target name.
+#
+# For example, if this project builds a library foo, and the namespace is
+# myproj, then the outside project can reference the library using myproj_foo
+# in its CMake code.
 install(EXPORT ${solutionName} NAMESPACE ${solutionName}_ DESTINATION cmake)
 
 `;
@@ -60,6 +93,10 @@ export function GenerateSrcDirectoryCMakeListsTxt(subprojects: Array<Project>): 
   ).join('\n');
 
   return `
+# This file is used to add all the subprojects to the build tree. The order
+# in which the projects are added matters. Place all dependencies before
+# parent projects.
+
 ${subdirectories}
 `;
 }
@@ -72,77 +109,101 @@ export function GenerateSubprojectCMakeListsTxt(
 
   let dependenciesList: string = thirdParty.map(
     function (dep: ThirdPartyProject): string {
-      return dep.name;
+      return `    ${dep.name}`;
     }
   ).concat(
     userSubProjects.map(
       function (userProject: Project): string {
-        return userProject.name;
+        return `    ${userProject.name}`;
       }
     )
     ).join('\n');
 
   let includeDirsList: string = thirdParty.map(
     function (dep: ThirdPartyProject): string {
-      return `\${${dep.name}_INCLUDE_DIRS}`;
+      return `    \${${dep.name}_INCLUDE_DIRS}`;
     }
   ).join('\n');
 
   return `
-cmake_minimum_required(VERSION 2.8.11)\n\n
+cmake_minimum_required(VERSION 2.8.11)
 
+# This variable is used to make it easy to copy-paste this entire file for
+# use in another project.
 set(TARGET ${project.name})
 
+# All header files should go here.
 set(HEADERS
-${ project.type !== ProjectType.Executable ?
+    ${ project.type !== ProjectType.Executable ?
    `${project.name}.h` : '' }
 )
 
+# All source files should go here.
 set(SOURCES
-  ${ project.type === ProjectType.Executable ?
-  'main.cpp': `${project.name}.cpp` }
+    ${ project.type === ProjectType.Executable ?
+    'main.cpp': `${project.name}.cpp` }
 )
 
 ${project.type !== ProjectType.Executable ? `
+# This option allows the builder to decide if this project should be built
+# as a static library or a shared library.
+option(${project.name}_STATIC "Build ${project.name} as a static library?" ${project.type === ProjectType.StaticLibrary? 'ON': 'OFF'})
 set(BUILD_TYPE )
 set(COMPILE_DEFINITIONS )
-option(${project.name}_STATIC "Build ${project.name} as a static library?" ${project.type === ProjectType.StaticLibrary? 'ON': 'OFF'})
+# Set variables to their appriate values
 if(${project.name}_STATIC)
   set(BUILD_TYPE "STATIC")
+  # Add any other static library specific compiler preprocessor flags here.
   set(COMPILE_DEFINITIONS ${project.name}_STATIC)
 else()
   set(BUILD_TYPE "SHARED")
+  # Add any other shared library specific compiler preprocessor flags here. 
   set(COMPILE_DEFINITIONS ${project.name}_EXPORT)
 endif()
 `: ''
 }
 
 ${ project.type == ProjectType.Executable ?
-  `add_executable(\${TARGET} \${HEADERS} \${SOURCES})`
+  `# Add the executable to the build using the headers and sources.
+add_executable(\${TARGET} \${HEADERS} \${SOURCES})`
   :
-  `add_library(\${TARGET} \${BUILD_TYPE} \${HEADERS} \${SOURCES})`
+  `# Add the library to the build the headers and sources.
+add_library(\${TARGET} \${BUILD_TYPE} \${HEADERS} \${SOURCES})`
 }
 
+# Add include directories here.
 target_include_directories(\${TARGET} PUBLIC
 ${includeDirsList}
 )
 
+# Add depenency libraries or CMake targets here.
 target_link_libraries(\${TARGET}
 ${dependenciesList}
 )
 
+# Add project-specific compiler preprocessor variables here.
 target_compile_definitions(\${TARGET} PUBLIC
 ${ project.type !== ProjectType.Executable ?
 '\${COMPILE_DEFINITIONS}' : ''
 }
 )
-${ project.type === ProjectType.Executable ?
-'set_target_properties(\${TARGET} PROPERTIES COMPILE_FLAGS "/MT" )':
-`# If necessary, uncomment the following line and add compiler flags 
-# set_target_properties(\${TARGET} PROPERTIES COMPILE_FLAGS )}` }
 
+# Add compiler flags here.
+set_target_properties(\${TARGET} PROPERTIES COMPILE_FLAGS "")
+
+# When using an MSVC compiler (Used by Visual Studio), link the
+# C Runtime Library as a static library.
+if(MSVC)
+  set_target_properties(\${TARGET} PROPERTIES LINK_FLAGS "/MT" )
+endif()
+
+# Place this target into its own solution folder.
 set_target_properties(\${TARGET} PROPERTIES FOLDER "\${TARGET}")
 
+# Add this project to the list of installed targets. When the install command
+# is invoked, this project will be placed in the installation directory. The
+# EXPORT part of this commands will cause CMake to allow this target to
+# be used by outside projects.
 install(TARGETS \${TARGET}
     EXPORT ${solutionName}
     COMPONENT ${ project.type == ProjectType.Executable ? "bin" : "lib"}
@@ -156,8 +217,7 @@ install(TARGETS \${TARGET}
 
 function GenerateExternalProjectAddBeginPartial(thirdParty: ThirdPartyProject) {
 
-  return `
-set(SOURCE_DIR \${CMAKE_CURRENT_BINARY_DIR}/${thirdParty.name})
+  return `set(SOURCE_DIR \${CMAKE_CURRENT_BINARY_DIR}/${thirdParty.name})
 ExternalProject_add(
     ${thirdParty.name}Download
     ${
@@ -188,12 +248,14 @@ set(${thirdParty.name}_BUILD_TYPE ${projectType})
 set(PREFIX lib)
 set(EXTENSION ${extension})
 if(WIN32)
-set(PREFIX )
-set(EXTENSION lib)
+    set(PREFIX )
+    set(EXTENSION lib)
 endif()
 add_library(${thirdParty.name} \${${thirdParty.name}_BUILD_TYPE} IMPORTED)
 add_dependencies(${thirdParty.name} ${thirdParty.name}Download ${dependenciesString})
 ${ thirdParty.type === ProjectType.Executable ? '' : `
+# NOTE: You may have to change the path specified after IMPORTED_LOCATION if you get
+# a linker error.
 set_target_properties(${thirdParty.name} PROPERTIES
     IMPORTED_LOCATION
         \${THIRD_PARTY_INSTALL_PREFIX}/lib/\${PREFIX}${thirdParty.name}.\${EXTENSION}
@@ -205,7 +267,7 @@ export function GenerateCMakeThirdPartyProjectString(
   cmakeProject: CMakeThirdPartyProject,
   dependencies: Array<string>): string {
 
-  let args: string = cmakeProject.cmakeArguments.join('\n');
+  let args: string = cmakeProject.cmakeArguments.join('\n\t\t');
   let beginPartial: string = GenerateExternalProjectAddBeginPartial(cmakeProject);
   let endPartial: string = GenerateExternalProjectAddEndPartial(cmakeProject, dependencies);
   return `
@@ -255,12 +317,41 @@ export function GenerateThirdPartyCMakeFile(
   });
 
   return `
+# This CMake file is used to download and build the third party dependencies
+# needed by this project. It is possible to obtain depencies from an archive
+# file, a git, mercurial, or even an svn repository, and these dependent
+# projects can either have CMake-based or script-based builds.
+#
+# To support all of that, this file uses CMake's ExternalProject module. This
+# provides a highly configurable way to obtain third party dependencies.
+#
+# For more advanced configuration options see
+# https://cmake.org/cmake/help/latest/module/ExternalProject.html
+#
+# The one shortcoming from this method is that it expects to the third
+# party dependency library to be in a specific location and have a specific
+# name. You will know this is the case because you will get a linker error
+# when buildng the project.
+#
+# Fortunately, the fix is easy: just change the path that comes after
+# IMPORTED_LOCATION for the library that produced the linker error.
+
 include(ExternalProject)
 
+# Set the installation prefix of all third party dependencies. After a build,
+# the libraries, include directories, and executables of all third party
+# projects will be placed in this directory.
 set(THIRD_PARTY_INSTALL_PREFIX \${CMAKE_CURRENT_BINARY_DIR}
     CACHE PATH "Directory to install 3rd party dependencies"
 )
+
+# Add the include/ subdirectory of the third party installation directory
+# to the list of directories to search for header files. This directory will
+# exist after the third party dependencies are built.
 include_directories(\${THIRD_PARTY_INSTALL_PREFIX}/include)
+
+# Third party dependencies start after this line. They are ordered so that
+# dependencies of parent projects appear first in the file.
 
 ${allDependencyStrings}
 `;
