8e2233d2214f91ab247057a610b9f32aaeaf9e49
Julio Gutierrez <juliog@knights.ucf.edu>
2017-05-14 21:30:51 -0400

    Changed webpack prod public path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/',
+    publicPath: '/CMakeStarter/',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
